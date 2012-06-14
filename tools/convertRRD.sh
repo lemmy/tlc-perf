c5a1bfe46581d8c0e015c8d45a6237577af0a952
Markus Alexander Kuppe <mkuppe@paradent-3.rennes.grid5000.fr>
2012-06-14 09:50:58 +0200

    Explicitly flush rrd to disk to keep latest data points

--- tools/convertRRD.sh
+++ tools/convertRRD.sh
@@ -9,6 +9,9 @@ OUT_DIR=$RESULT_DIR/`hostname -s`
 
 mkdir -p $OUT_DIR
 
+# explicitly flush rrd data to disk by regenerating munin graphs locally
+sudo -u munin /usr/bin/munin-cron
+
 # locally
 for RRD in `find /var/lib/munin/tlc/*.rrd`;
 do
