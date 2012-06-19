ebe956ca2727570744b3109a351d125b18cfb879
Markus Alexander Kuppe <tlaplus.net@lemmster.de>
2012-06-19 21:08:21 +0200

    /home/mkuppe/grid5000.git/results/l10_n6-w002-390954-19:05:13

--- cloud/G5k/deployAndRun.sh
+++ cloud/G5k/deployAndRun.sh
@@ -2,5 +2,5 @@
 
 kadeploy3 -e squeeze-x64-nfs -o /home/mkuppe/$OAR_JOB_ID.nodes -f $OAR_FILE_NODES -k
 taktuk -l root -s -o connector -o status -o output='"$host: $line\n"' -f /home/mkuppe/$OAR_JOB_ID.nodes broadcast exec [ "/home/mkuppe/grid5000.git/cloud/G5k/Provision.sh" ]
-ssh `head -n 1 /home/mkuppe/$OAR_JOB_ID.nodes` "/home/mkuppe/grid5000.git/cloud/G5k/tlc.sh $1 $2 $3 $4 $OAR_JOB_ID /home/mkuppe/$OAR_JOB_ID.nodes"
+ssh `head -n 1 /home/mkuppe/$OAR_JOB_ID.nodes` "/home/mkuppe/grid5000.git/tlc.sh $1 $2 $3 $4 $OAR_JOB_ID /home/mkuppe/$OAR_JOB_ID.nodes"
 
---
+++ tlc.sh
@@ -0,0 +1,136 @@
+#!/bin/bash
+
+# Parameters
+# 1: Start (Num of workers)
+# 2: End   (Num of workers)
+# 3: Stepping (Increment of workers)
+# optional
+# 4: Model names
+# 5: JOB_ID (Identifier for current run)
+# 6: OAR_FILE_Nodes (ascii file with list of all nodes (one node per line))
+# 7: ROOT_DIR (location of git repo)
+
+## Debugging
+set -x
+
+## ID of this script
+PID=$$
+
+START=$1
+END=$2
+STEP=$3
+
+TIMESTAMP=`date -u +%T`
+
+## grid job identifier
+JOB_ID=${5-$OAR_JOB_ID}
+
+## which models to check
+MODEL_NAMES=${4-"l12_n6 l14_n6"}
+
+## root dir
+ROOT_DIR=${7-/home/mkuppe/grid5000.git}
+
+## where the model _files_ are kept
+SPEC_PATH=$ROOT_DIR/models/
+
+## tools not available in the grid
+JAVA_PATH=$ROOT_DIR/tools/jre/bin/java
+PSSH_PATH=$ROOT_DIR/tools/pssh/bin/pssh
+CONVERTRRD_PATH=$ROOT_DIR/tools/convertRRD.sh
+
+## local tools in the grid
+UNZIP_PATH=/usr/bin/unzip
+GIT_PATH=/usr/bin/git
+
+## staging area to reduce load on NFS
+TARGET_DIR=/tmp/$PID
+TARGET_TLA_DIR=$TARGET_DIR/tla
+TARGET_SPEC_DIR=$TARGET_DIR/spec
+
+## go to root
+cd $ROOT_DIR
+
+## cleanup old leftovers
+rm -rf $TARGET_SPEC_DIR
+rm -rf $TARGET_TLA_DIR
+
+## extract tla.zip distribution to (local) target directory for server
+$UNZIP_PATH -q $ROOT_DIR/dist/tla.zip -d $TARGET_DIR/
+
+## copy spec to target directory
+cp -a $SPEC_PATH $TARGET_SPEC_DIR
+
+## select server (first host in the list)
+SERVER_NAME=`hostname -f`
+
+## list of hosts (host appears multiple times for each core)
+FILE_NODES=$TARGET_DIR/$JOB_ID-hosts.txt
+cat ${6-$OAR_FILE_NODES} |grep -v $HOSTNAME | uniq | sort > $FILE_NODES
+
+## loop over models
+for MODEL_NAME in $MODEL_NAMES;
+do
+    echo "checking model: $MODEL_NAME"
+
+    ## loop over workers
+    for ((WORKER_COUNT=$START ; $WORKER_COUNT <= $END; WORKER_COUNT = $WORKER_COUNT $STEP));
+    do
+	echo "with workers: $WORKER_COUNT"
+
+	##
+	WORKER_COUNT_PADDED=$(printf %03d $WORKER_COUNT)
+	RESULT_DIR=$ROOT_DIR/results/$MODEL_NAME-w$WORKER_COUNT_PADDED-$JOB_ID-$TIMESTAMP
+	
+	##
+	## write job information
+        
+        ## create result directory
+	mkdir -p $RESULT_DIR
+	
+	##
+	## spawn workers in the bg with pssh (they wait for the server)
+	
+	## create a worker file for pssh
+	WORKER_FILE=$TARGET_DIR/$JOB_ID-w$WORKER_COUNT-m$MODEL_NAME.txt
+	tail -$WORKER_COUNT $FILE_NODES > $WORKER_FILE
+	
+	## spawn pssh process
+	$PSSH_PATH -t -1 -p $WORKER_COUNT -h $WORKER_FILE $JAVA_PATH -Xmx2096m -cp $ROOT_DIR/dist/tla2tools.jar tlc2.tool.distributed.TLCWorker $SERVER_NAME &
+
+	##
+	## spawn server in fg
+        
+        ## log amount of workers to result directory
+	echo $WORKER_COUNT > $RESULT_DIR/worker_count.txt
+	cat $WORKER_FILE > $RESULT_DIR/workers.txt
+	
+	echo `hostname -f` > $RESULT_DIR/server.txt
+
+        ## log start timestamp to result directory
+	echo `date -u +%T` > $RESULT_DIR/start_time.txt
+
+        ## spawn Java VM with server
+        $JAVA_PATH -Xmx2096m -cp $TARGET_TLA_DIR:$TARGET_TLA_DIR/lib/aspectjrt.jar -javaagent:$TARGET_TLA_DIR/lib/aspectjweaver.jar -Dorg.aspectj.weaver.showWeaveInfo=false -Daj.weaving.verbose=false -Dtlc2.tool.distributed.TLCStatistics.path=$RESULT_DIR/ -Djava.rmi.server.logCalls=false -Dtlc2.tool.distributed.TLCServer.expectedWorkerCount=$WORKER_COUNT tlc2.tool.distributed.TLCServer $TARGET_SPEC_DIR/$MODEL_NAME.tla 2>&1 | tee $RESULT_DIR/server.out
+
+        ## log start timestamp to result directory
+	echo `date -u +%T` > $RESULT_DIR/end_time.txt
+
+	## backup rrd data (remotely and locally)
+	$PSSH_PATH -t -1 -p $WORKER_COUNT -h $WORKER_FILE $CONVERTRRD_PATH $RESULT_DIR
+	# locally
+	$CONVERTRRD_PATH $RESULT_DIR
+
+	##
+	## persistently store result (implicitly like a sleep letting workers/server shutdown)
+	#$GIT_PATH pull origin master
+	$GIT_PATH add dist/tla.zip
+	$GIT_PATH add dist/tla2tools.jar
+	$GIT_PATH add $RESULT_DIR/*
+	find $RESULT_DIR -type d | xargs -I {} $GIT_PATH add {}/*
+	$GIT_PATH commit -m ''$RESULT_DIR''
+	#$GIT_PATH push origin master
+    done
+done
+
+
