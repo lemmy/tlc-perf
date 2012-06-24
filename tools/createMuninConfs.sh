a5d200b506041ba98533392ad880b723161b196a
Markus Alexander Kuppe <tlaplus.net@lemmster.de>
2012-06-24 15:58:53 +0000

    Collect worker nodes centrally on the master

--- tools/createMuninConfs.sh
+++ tools/createMuninConfs.sh
@@ -5,6 +5,7 @@ NODES=$1
 for node in `cat $1`
 do
     touch /tmp/$node.conf
-    echo "[$node] address $node" > /tmp/$node.conf
+    echo "[$node]" > /tmp/$node.conf
+    echo " address $node" >> /tmp/$node.conf
     sudo mv /tmp/$node.conf /etc/munin/munin-conf.d/
 done
