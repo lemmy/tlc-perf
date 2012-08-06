064ea09face075b77db42011b58c07a1b5123650
Markus Alexander Kuppe <tlaplus.net@lemmster.de>
2012-08-06 16:31:39 +0200

    Print manual steps at the end

--- cloud/deployJenkins.sh
+++ cloud/deployJenkins.sh
@@ -6,12 +6,15 @@ sh -c 'echo deb http://pkg.jenkins-ci.org/debian-stable binary/ > /etc/apt/sourc
 aptitude update
 aptitude install jenkins -y
 
+# Stop jenkins to tweak configuration
+service jenkins stop
+
 # Set jenkins prefix which matches the URL under which apache is going to proxy jenkins
 sed -i 's/PREFIX=\/jenkins/PREFIX=\/build/g' /etc/default/jenkins
 sed -i 's/JENKINS_ARGS="--webroot=\/var\/cache\/jenkins\/war --httpPort=$HTTP_PORT --ajp13Port=$AJP_PORT"/JENKINS_ARGS="--webroot=\/var\/cache\/jenkins\/war --httpPort=$HTTP_PORT --ajp13Port=$AJP_PORT --prefix=$PREFIX"/g' /etc/default/jenkins
 
 # Set jenkins home to reside on node local storage
-mkdir -p /mnt/jenkins/plugins/
+mkdir -p /mnt/jenkins/plugins/scm-sync-configuration/checkoutConfiguration
 sed -i 's/JENKINS_HOME=\/var\/lib\/jenkins/JENKINS_HOME=\/mnt\/jenkins/g' /etc/default/jenkins
 
 # Install necessary plugins
@@ -27,25 +30,24 @@ wget -q http://updates.jenkins-ci.org/latest/bugzilla.hpi
 wget -q http://updates.jenkins-ci.org/latest/promoted-builds.hpi
 cd -
 
-# Finally make sure everything belong to jenkins user
+# Finally make sure everything belong to jenkins user before we restart jenkins
 chown -R jenkins:nogroup /mnt/jenkins
 
-# Apply jenkins conf somehow?!
-echo "TODO!!!
-- ThinBackup
-- Jenkins plugings"
+# Restart jenkins to pick up config changes
+service jenkins restart
 
 # "Reuse" tla git repo provisioned for user kuppe
 # Jenkins consumes it as file:///home/kuppe/tla.git
 ln -s /home/kuppe/git/tla/ /home/kuppe/tla.git
 
-# Restart jenkins to pick up config changes
-service jenkins restart
 
 #
 # Activate Apache2 proxy modules
 a2enmod proxy
 a2enmod proxy_http
+
+# Set up SSL to protect network communication
+echo "TODO!!! (where to get the private key from)"
 
 # Set up Apache to proxy the Jenkins server under build/ URL
 echo "
@@ -63,4 +65,8 @@ echo "
 
 # Restart Apache to pick up config changes
 service apache2 restart
+
+# Manually pull jenkins config in from private git repo (it's done manually to not store the private keys publically)
+echo "As user jenkins, set up ~/.ssh/config to reach taurus.mkce.de"
+echo "and clone git@taurus:tla/build afterwards into /mnt/jenkins/scm-sync-configuration/checkoutConfiguration/"
 
