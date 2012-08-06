ebb3762cfe09874ac04747b63245dc2dda3293fc
Markus Alexander Kuppe <tlaplus.net@lemmster.de>
2012-08-06 15:57:46 +0200

    Install necessary plugins automatically

--- cloud/deployJenkins.sh
+++ cloud/deployJenkins.sh
@@ -11,9 +11,24 @@ sed -i 's/PREFIX=\/jenkins/PREFIX=\/build/g' /etc/default/jenkins
 sed -i 's/JENKINS_ARGS="--webroot=\/var\/cache\/jenkins\/war --httpPort=$HTTP_PORT --ajp13Port=$AJP_PORT"/JENKINS_ARGS="--webroot=\/var\/cache\/jenkins\/war --httpPort=$HTTP_PORT --ajp13Port=$AJP_PORT --prefix=$PREFIX"/g' /etc/default/jenkins
 
 # Set jenkins home to reside on node local storage
-mkdir /mnt/jenkins
-chown jenkins:nogroup /mnt/jenkins
+mkdir -p /mnt/jenkins/plugins/
 sed -i 's/JENKINS_HOME=\/var\/lib\/jenkins/JENKINS_HOME=\/mnt\/jenkins/g' /etc/default/jenkins
+
+# Install necessary plugins
+cd /mnt/jenkins/plugins
+wget -q http://updates.jenkins-ci.org/latest/thinBackup.hpi
+wget -q http://updates.jenkins-ci.org/latest/git.hpi
+wget -q http://updates.jenkins-ci.org/latest/scm-sync-configuration.hpi
+wget -q http://updates.jenkins-ci.org/latest/subversion.hpi
+wget -q http://updates.jenkins-ci.org/latest/xvnc.hpi
+wget -q http://updates.jenkins-ci.org/latest/ec2.hpi
+wget -q http://updates.jenkins-ci.org/latest/ws-cleanup.hpi
+wget -q http://updates.jenkins-ci.org/latest/bugzilla.hpi
+wget -q http://updates.jenkins-ci.org/latest/promoted-builds.hpi
+cd -
+
+# Finally make sure everything belong to jenkins user
+chown -R jenkins:nogroup /mnt/jenkins
 
 # Apply jenkins conf somehow?!
 echo "TODO!!!
